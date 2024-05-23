75b44d9e28048a28f3943ee56066836711383c2f
Hussain Bhavnagarwala <hussainsail2002@gmail.com>
2024-05-22 21:14:41 -0700

    Update AG_OC_pontry_main.py

--- AG_OC_pontry_main.py
+++ AG_OC_pontry_main.py
@@ -10,6 +10,7 @@ nx=4 # number of state
 nu=2 # number of control
 Tfinal = 5 # final time 
 Nt =int((Tfinal/h)+1) # number of time steps
+M = 20 # number of perturbed trajectories  
 
 x0=np.array([[10],[15],[2],[0]]) # Initial state:
 u=np.random.default_rng().standard_normal(size=(nu,Nt-1)) # Assume random control values
@@ -17,15 +18,14 @@ x=np.zeros([nx,Nt]) #initialize the state vector
 x[:,0:1]=x0 
 
 #Load the ilqr class
-ilqr=Ilqr(nx,nu,Nt,h)
+ilqr=Ilqr(nx,nu,Nt,h,M)
 x = ilqr.forward_rollout(x,u)
-J = ilqr.cost_function(x,u)
 
 #ilqr / DDP algorithim
 # We do the forward pass in this codeblock
 
-u_fin=Ilqr.forward_pass()
-x_fin=Ilqr.forward_rollout(x,u_fin)
+u_fin=ilqr.forward_pass(x,u)
+x_fin=ilqr.forward_rollout(x,u_fin)
 
 if os.path.exists('A_matrix_V5_online.pth'):
     os.remove('A_matrix_V5_online.pth')
@@ -36,7 +36,7 @@ if os.path.exists('B_matrix_V5_online.pth'):
 if os.path.exists('ml_trainingdata_V4_R2_A.csv'):
     os.remove('ml_trainingdata_V4_R2_A.csv')
 
-plt.plot(x[0,:],x[1,:],color='b')
+plt.plot(x_fin[0,:],x_fin[1,:],color='b')
 plt.title('X and Y axis position of Bicycle')
 plt.xlabel('X axis Position ')
 plt.ylabel('Y axis Position')
